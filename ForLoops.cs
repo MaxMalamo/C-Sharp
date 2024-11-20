d77c2ac279cb5cca3a91ee6b237f5cb5e70af88c
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-11-20 10:12:14 +0200

    Add files via upload

---
+++ ForLoops.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Loop Example 
+            /*
+            for(int i = 0; i < 6;  i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i = 1; i <= 10; i +=2)
+            {
+                Console.WriteLine(i);
+            }
+            */
+            //Loop Example 2
+            /*
+            Console.Write("How many times do you want to loop? ");
+            int loopCounter = Convert.ToInt32(Console.ReadLine());
+            if (loopCounter > 0)
+            {
+                Console.WriteLine("Sorry, Please enter a value above 0");
+            }
+            else
+            {
+                for (int i = 0; i < loopCounter; i++)
+                {
+                    Console.WriteLine("Loop: " + i);
+                }
+            }
+            */
+            Console.Write("How do you want to repeat? ");
+            string message = Console.ReadLine();
+
+            Console.Write("How many times do you want to repeat the message? ");
+            int loopCounter = Convert.ToInt32(Console.ReadLine());
+
+            if (loopCounter <= 0)
+            {
+                Console.WriteLine("Sorry, Please enter a value above 0");
+            }
+            else
+            {
+                for (int i = 0; i < loopCounter; i++)
+                {
+                    Console.WriteLine(message);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringChars.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Bob";
+            char letter = 'a';
+
+            Console.WriteLine("Your name is ");
+            Console.Write(name);
+
+            Console.Write("\n Your letter is ");
+            Console.Write(letter);
+
+            Console.ReadKey(); 
+
+        }
+    }
+}
