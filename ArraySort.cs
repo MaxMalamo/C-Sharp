f48b739254b1c1c0c843041e176108c24afc3538
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-11-26 09:36:14 +0200

    Add files via upload

---
+++ ArraySort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Sort Array numbers
+            int[] numbers = { 10, 2, 30, 46, 5, 6, 78, 800, 9 };
+
+            Array.Sort(numbers);
+
+            foreach(int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
