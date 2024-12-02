6aeea848e19cc2af19441a4bdfdf6c9c07bb416e
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-12-02 13:37:06 +0200

    Add files via upload

---
+++ FunctionParameters.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int calc = Add(50, 50);
+            Console.WriteLine(calc);
+
+            int num = ReadInt("Enter a number: ");
+            int num2 = ReadInt("Enter a second number: ");
+
+            int result = Add(num, num2);
+            Console.WriteLine(result);
+
+            string name = ReadString("Enter your name: ");
+            int age = ReadInt("Enter your age: ");
+            string details = UserDetails(name, age);
+            Console.WriteLine(details);
+            Console.ReadLine();
+        }
+
+        static string UserDetails(string name, int age)
+        {
+            return $"Hello my name is {name} and my age is {age}!";
+        }
+
+        static string ReadString(string message)
+        {
+            Console.Write($"{message} ");
+            return Console.ReadLine();
+        }
+
+        static int ReadInt(string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
