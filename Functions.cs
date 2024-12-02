675adf61ceebf393433fe2fa1b2341be06e1142a
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-12-02 12:34:57 +0200

    Add files via upload

---
+++ Functions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions // Defines the namespace for the program
+{
+
+    class test // Class names test containing a static method
+    {
+        public static void something() // Static method called "Something" that prints a blank line to teh console
+        {
+            Console.WriteLine("Test");
+            Console.WriteLine();
+        }
+    }
+    
+    internal class Program // Main Program
+    {
+        
+        static void Main(string[] args) // The start point of the program
+        {
+            // Calls the static method "something" from the test class
+            test.something();
+
+            WelcomeMessage();
+
+            // Keeps console open until next press
+            Console.ReadLine();
+        }
+
+        // Static method that prints a welcome message to the console
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome let's code some C#");
+        }
+    }
+}
